959f8ed05edec37d7fcdffc0db8091559eeb46b3
FlexonyoPizza <LeonDhoska@gmail.com>
2026-09-09 09:54:37 -0400

    attestation tests

---
+++ lib/davinci_pas_test_kit/client/v2.2.1/pas_client_attestations_group.rb
@@ -0,0 +1,34 @@
+require_relative 'attestations/attestation_instructions'
+require_relative 'attestations/client_clinical_data_review_attestation_test'
+require_relative 'attestations/client_data_element_guidance_attestation_test'
+require_relative 'attestations/client_omitted_element_tolerance_attestation_test'
+require_relative 'attestations/client_provider_review_permitted_attestation_test'
+require_relative 'attestations/client_standard_data_element_use_attestation_test'
+
+module DaVinciPASTestKit
+  module DaVinciPASV221
+    class PASClientAttestationsGroup < Inferno::TestGroup
+      id :pas_client_v221_attestations
+      title 'Visual Inspection and Attestation'
+      description %(
+        Each test in this group asks the tester to confirm system conformance to a
+        requirement from the [PAS v2.2.1 Implementation Guide](https://hl7.org/fhir/us/davinci-pas/2.2.1).
+        The tester attests that the client system under test meets the statement by selecting "Yes",
+        and may record supporting details in the accompanying notes field. Selecting "No" fails the
+        test. Notes provided are recorded in the test result.
+
+        Testers are responsible for confirming that their system meets all requirements associated with a test
+        before selecting "Yes" on the attestation input with the same name as the test. The text of the
+        attested requirement(s) for each input can be reviewed by clicking the "View Specification Requirements" link
+        in the "About" tab of the test with the same name.
+      )
+      input_instructions ATTESTATION_INPUT_INSTRUCTIONS
+
+      test from: :pas_client_v221_omitted_element_tolerance_attestation
+      test from: :pas_client_v221_standard_data_element_use_attestation
+      test from: :pas_client_v221_data_element_guidance_attestation
+      test from: :pas_client_v221_provider_review_permitted_attestation
+      test from: :pas_client_v221_clinical_data_review_attestation
+    end
+  end
+end
